48952a73b438a6acde547977ee3918cc13fd748b
Woo <swy831@gmail.com>
2025-03-10 16:41:18 +0900

    feat : 카테고리 음료, 디저트 추가

--- src/kiosk/Kiosk.java
+++ src/kiosk/Kiosk.java
@@ -1,31 +1,52 @@
 package kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
 
-    private Menu burgerMenu;
+    private List<Menu> menus = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
 
-    public Kiosk(Menu burgerMenu) {
-        this.burgerMenu = burgerMenu;
+    public Kiosk() {
+
+    }
+
+    public void addKioskMenu(Menu menu){
+        menus.add(menu);
     }
 
     public void start() {
-        Scanner scanner = new Scanner(System.in);
-
+        int menuChoose;
 
         while (1 == 1) {
             System.out.println("[ SHAKESHACK MENU ]");
-            for (int i = 0; i < burgerMenu.getMenuItems().size(); i++) {
-                System.out.println(i + ". " + burgerMenu.getMenuItems().get(i).name + "  |  ￦ " + burgerMenu.getMenuItems().get(i).price + "  |  " + burgerMenu.getMenuItems().get(i).content);
+
+            for(int i=0; i<menus.size(); i++)
+            {
+                System.out.println(i+1 + ". " + menus.get(i).getCategoryName());
             }
             System.out.println("0. 종료");
-            int menuChoose = scanner.nextInt();
+            menuChoose = scanner.nextInt();
             if (menuChoose == 0) {
                 break;
             } else {
-//                System.out.println("선택한 메뉴 : " + menu.menuItems.get(menuChoose).name + " " + menu.menuItems.get(menuChoose).price + " " + menu.menuItems.get(menuChoose).content);
+                menuChoose = menuChoose - 1;
+                List<MenuItem> itemList = menus.get(menuChoose).getMenuItems();
+                for(int i=0; i<itemList.size(); i++ )
+                {
+                     System.out.println(i+1 + ". " + itemList.get(i).name + " | ₩ " + itemList.get(i).price + " | " + itemList.get(i).content + " | ");
+                 }
+                System.out.println("0. 뒤로가기");
+                menuChoose = scanner.nextInt();
+                if (menuChoose == 0) {
+                    continue;
+                }
+                else{
+                    menuChoose = menuChoose - 1;
+                    System.out.println("선택한 메뉴 : " + itemList.get(menuChoose).name + " | ₩" + itemList.get(menuChoose).price + " | " + itemList.get(menuChoose).content);
+                }
             }
         }
     }
